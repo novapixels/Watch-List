f7bbbc37383786dca66c70e6098fe892c17bbd5b
monica randriamialy <mo@novapixels.net>
2021-11-11 15:53:50 +0100

    controllers ok

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,33 @@
 class ListsController < ApplicationController
-  def show
+  before_action :set_list, only: [:show]
+
+  def index
     @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list), notice: '🙌 Your list was successfully created!'
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
