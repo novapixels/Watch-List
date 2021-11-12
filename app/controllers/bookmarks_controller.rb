e3dfd288a90fe03b4c7be4aee63d3ca389bd0a19
monica randriamialy <mo@novapixels.net>
2021-11-12 12:33:25 +0100

    heroku push

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,29 +1,61 @@
 class BookmarksController < ApplicationController
-  def new
-    @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new
-  end
-
   def create
-    @list = List.find(params[:list_id])
+    @movies = Movie.all
+    @movie = Movie.select { |movie| movie.title == params[:bookmark][:movie_id] }[0]
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.movie = @movie
+    @list = List.find(params[:list_id])
     @bookmark.list = @list
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render :new
+      render 'lists/show'
     end
   end
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to @bookmark.list, notice: 'Bookmark successfully deleted'
+    redirect_to list_path(@bookmark.list)
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:comment)
   end
 end
+
+# class BookmarksController < ApplicationController
+#   def index
+#     @bookmarks = Bookmark.all
+#   end
+
+#   def new
+#     @list = List.find(params[:list_id])
+#     @bookmark = Bookmark.new
+#   end
+
+#   def create
+#     @list = List.find(params[:list_id])
+#     @bookmark = Bookmark.new(bookmark_params)
+#     @bookmark.list = @list
+#     if @bookmark.save
+#       redirect_to list_path(@list)
+#     else
+#       render :new
+#     end
+#   end
+
+#   def destroy
+#     @bookmark = Bookmark.find(params[:id])
+#     @bookmark.destroy
+#     redirect_to @bookmark.list, notice: 'Bookmark successfully deleted'
+#   end
+
+#   private
+
+#   def bookmark_params
+#     params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+#   end
+# end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,17 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show]
-
   def index
     @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
     @bookmark = Bookmark.new
+    @movies = Movie.all
   end
 
   def new
     @list = List.new
+    # add the movies to the list
   end
 
   def create
@@ -23,11 +24,49 @@ class ListsController < ApplicationController
   end
 
   private
-  def set_list
-    @list = List.find(params[:id])
-  end
 
   def list_params
     params.require(:list).permit(:name)
   end
 end
+
+# class ListsController < ApplicationController
+#   before_action :set_list, only: [:show]
+
+#   def index
+#     @lists = List.all
+#     @list = List.new
+#     if @list.save
+#       redirect_to list_path(@list), notice: '🙌 Your list was successfully created!'
+#     else
+#       render :new
+#     end
+#   end
+
+#   def show
+#     @lists = List.all
+#     @bookmark = Bookmark.all
+#   end
+
+#   def new
+#     @list = List.new
+#   end
+
+#   def create
+#     @list = List.new(list_params)
+#     if @list.save
+#       redirect_to list_path(@list), notice: '🙌 Your list was successfully created!'
+#     else
+#       render :new
+#     end
+#   end
+
+#   private
+#   def set_list
+#     @list = List.find(params[:id])
+#   end
+
+#   def list_params
+#     params.require(:list).permit(:name)
+#   end
+# end
